c1b9935b918cbc7b358ebb519c0e4309d0e05dc2
Adolligit <adelson.onod@gmail.com>
2022-12-03 22:18:25 -0300

    feat: incorporando o 'swagger' no projeto

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 const ControllerErrorHandler = require('./Errors/ControllerErrorHandler');
 const Product = require('./routes/Product');
 const Sale = require('./routes/Sale');
@@ -6,17 +9,15 @@ const Sale = require('./routes/Sale');
 const app = express();
 const endpoints = ['/products', '/sales'];
 
-// não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use(endpoints[0], Product);
 app.use(endpoints[1], Sale);
 app.use(ControllerErrorHandler);
 
-// não remova essa exportação, é para o avaliador funcionar
-// você pode registrar suas rotas normalmente, como o exemplo acima
-// você deve usar o arquivo index.js para executar sua aplicação 
 module.exports = app;

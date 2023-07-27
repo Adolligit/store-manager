2b33317e1c5bd2b5e46f8c7fc8aa3b6cf166335c
Adolligit <adelson.onod@gmail.com>
2023-07-27 17:17:38 -0300

    feat [index]: adicionando versão para a API

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const express = require('express');
 const app = require('./app');
 require('dotenv').config();
 
 const PORT = process.env.APP_PORT;
 
-app.listen(PORT || 3000, () => {
+const index = express();
+
+index.use('/v1', app)
+index.listen(PORT || 3000, () => {
   console.log(`Escutando na porta ${PORT}`);
 });

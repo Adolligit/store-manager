fe243850cf21eb338be83ad2a8c50c55b2288694
Adolligit <adelson.onod@gmail.com>
2023-07-28 19:40:55 -0300

    chore [app]: tirando endpoint para os antigos testes

--- app.js
+++ app.js
@@ -2,11 +2,6 @@ const app = require('express').Router();
 const routes = require('./route');
 const middleware = require('./middleware');
 
-app.get('/', (_request, response) => {
-  response.send();
-});
-
-
 app.use('/v1', routes);
 app.use(middleware.handlerError.controller);
 
